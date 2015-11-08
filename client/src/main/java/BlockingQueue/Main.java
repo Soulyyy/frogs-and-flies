52aefbe30020ebdd3099ef92c3c749171e44f38b
soulyyy <soulyyy@gmail.com>
2015-11-08 23:51:50 +0200

    deleted dead code

--- client/src/main/java/BlockingQueue/Main.java
+++
@@ -1,7 +0,0 @@
-package BlockingQueue;
-
-/**
- * Created by Hans on 16/10/2015.
- */
-public class Main {
-}
--- client/src/main/java/visualization/GameBoardVisualization.java
+++
@@ -1,7 +0,0 @@
-package visualization;
-
-/**
- * Created by Hans on 27/10/2015.
- */
-public class GameBoardVisualization {
-}
